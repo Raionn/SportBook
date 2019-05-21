58042545272b3687f41decd148e9dd0e33bdca22
Mindaugas Laskis <mindaugas.laskis@gmail.com>
2019-05-21 16:36:58 +0300

    user navigation

---
+++ SportBook/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportBook.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult UserMain()
+        {
+            return View();
+        }
+        public IActionResult PlayerListWindow()
+        {
+            return View();
+        }
+        public IActionResult UserProfileWindow()
+        {
+            return View();
+        }
+    }
+}
