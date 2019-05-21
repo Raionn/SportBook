166c9404bcc8fe752432368d80eb6b861567e81c
Raionn <arturas9631@gmail.com>
2019-05-22 00:30:12 +0300

    Finished event form and cancel button added

--- SportBook/Controllers/EventController.cs
+++ SportBook/Controllers/EventController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+using SportBook.Models;
 
 namespace SportBook.Controllers
 {
@@ -10,6 +13,7 @@ namespace SportBook.Controllers
     {
         public IActionResult EventList()
         {
+            //TODO: getEventList()
             IList<string> events = new List<string>
             {
                 "event_1",
@@ -23,15 +27,57 @@ namespace SportBook.Controllers
             return View();
         }
 
-        public IActionResult EventForm()
+        public ActionResult cancelEvent(string id)
         {
+            //TODO: deleteEvent(id)
+            
+            return RedirectToAction("EventList");
+        }
+
+        public IActionResult EventForm(string id)
+        {
+            ViewData["id"] = id;
 
             return View();
         }
 
-        public IActionResult EventWindow()
+        public IActionResult EventWindow(string id)
         {
+            ViewData["id"] = id;
             return View();
+        }
+
+        public ActionResult submitChanges(string id)
+        {
+            //TODO: validate
+
+            string name = String.Format("{0}", Request.Form["name"]);
+            var game = String.Format("{0}", Request.Form["selectedGame"]);
+            //if (Request.Form["datetimepicker"].Equals(null))
+            //{
+            //    return RedirectToAction("EventForm", "Event", new { id = "error"});
+            //}
+            try
+            {
+                var date = Convert.ToDateTime(String.Format(Request.Form["datetimepicker"]));
+
+            }
+            catch
+            {
+                //display error
+                return RedirectToAction("Error");
+            }
+
+            //TODO: update event
+
+
+            return RedirectToAction("EventWindow", "Event", new { id });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
